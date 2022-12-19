0a2c83ad02dd453c27b0df99e390d1a2b5a4abd7
Tony Zhao <tawnyzhao@gmail.com>
2022-12-19 15:13:46 -0800

    Add ray class

---
+++ src/ray.cc
@@ -0,0 +1,12 @@
+#include "ray.h"
+
+Ray::Ray() {}
+Ray::Ray(const Point3& origin, const Vec3& direction)
+    : orig(origin), dir(direction) {}
+
+Point3 Ray::origin() const { return orig; }
+Vec3 Ray::direction() const { return dir; }
+
+Point3 Ray::at(double t) const {
+    return orig + t*dir;
+}
