2e415f88f43653dae615e03976e0d8402cae2766
Tony Zhao <tawnyzhao@gmail.com>
2022-12-26 01:55:14 -0800

    Add dielectric material

---
+++ src/dielectric.rs
@@ -0,0 +1,51 @@
+use crate::{
+    material::Material,
+    vec3::{reflect, refract, unit_vector, Color, Vec3, dot}, utils::random_double, ray::Ray,
+};
+
+pub struct Dielectric {
+    index_of_refraction: f64,
+}
+
+impl Dielectric {
+    pub fn new(index_of_refraction: f64) -> Self {
+        Self {
+            index_of_refraction,
+        }
+    }
+    pub fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+        let r0 = ((1.0 - ref_idx) / (1.0 + ref_idx)).powi(2);
+        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+    }
+}
+
+impl Material for Dielectric {
+    fn scatter(
+        &self,
+        r_in: &crate::ray::Ray,
+        rec: &crate::hittable::HitRecord,
+        attenuation: &mut crate::vec3::Color,
+        scattered: &mut crate::ray::Ray,
+    ) -> bool {
+        let refraction_ratio = if rec.front_face {
+            1.0 / self.index_of_refraction
+        } else {
+            self.index_of_refraction
+        };
+
+        let unit_direction = unit_vector(r_in.direction());
+        let cos_theta = f64::min(dot(-unit_direction, rec.normal), 1.0);
+        let sin_theta = f64::sqrt(1.0 - cos_theta.powi(2));
+
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let direction = if cannot_refract || Dielectric::reflectance(cos_theta, refraction_ratio) > random_double(0.0, 1.0) {
+            reflect(unit_direction, rec.normal)
+        } else {
+            refract(unit_direction, rec.normal, refraction_ratio)
+        };
+
+        attenuation.clone_from(&Color::new(1.0, 1.0, 1.0));
+        scattered.clone_from(&Ray::new(rec.p, direction));
+        true
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
 use std::rc::Rc;
 
-use hittable::{Hittable, HitRecord};
+use hittable::{HitRecord, Hittable};
 use ray::Ray;
-use vec3::{unit_vector, random_in_unit_sphere, random_unit_vector};
+use vec3::{random_in_unit_sphere, random_unit_vector, unit_vector};
 
 use crate::{
-    camera::Camera, color::write_color, hittable::HittableList, sphere::Sphere, vec3::{Vec3, Point, Color}, utils::random_double, lambertian::Lambertian, material::Material, metal::Metal,
+    camera::Camera,
+    color::write_color,
+    hittable::HittableList,
+    lambertian::Lambertian,
+    material::Material,
+    metal::Metal,
+    sphere::Sphere,
+    utils::random_double,
+    vec3::{Color, Point, Vec3}, dielectric::Dielectric,
 };
 
 mod camera;
 mod color;
+mod dielectric;
 mod hittable;
+mod lambertian;
+mod material;
+mod metal;
 mod ray;
 mod sphere;
 mod utils;
 mod vec3;
-mod material;
-mod lambertian;
-mod metal;
 
 fn ray_color(ray: &Ray, world: &dyn Hittable, depth: i32) -> Color {
     let mut rec: HitRecord = Default::default();
@@ -30,35 +39,56 @@ fn ray_color(ray: &Ray, world: &dyn Hittable, depth: i32) -> Color {
         let mut attenuation: Color = Default::default();
         if let Some(ref mat) = rec.material {
             if mat.scatter(ray, &rec, &mut attenuation, &mut scattered) {
-                return attenuation * ray_color(&scattered, world, depth-1);
+                return attenuation * ray_color(&scattered, world, depth - 1);
             }
         }
-        return Color::new(0.0,0.0,0.0);
+        return Color::new(0.0, 0.0, 0.0);
     }
     let unit_direction = unit_vector(ray.direction());
-    let t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0 - t)*Color::new(1.0, 1.0, 1.0) + t*Color::new(0.5, 0.7, 1.0);
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0);
 }
 
 fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 1000;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
-    let samples_per_pixel = 100;
+    let samples_per_pixel = 200;
     let max_depth = 50;
     // materials
     let matte_green: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let matte_red: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
-    let metal_grey: Rc<dyn Material> = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.01));
+    let matte_purple: Rc<dyn Material> = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.7)));
+    let metal_grey: Rc<dyn Material> = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.05));
     let metal_blue: Rc<dyn Material> = Rc::new(Metal::new(Color::new(0.2, 0.2, 0.7), 1.0));
-
-
+    let glass: Rc<dyn Material> = Rc::new(Dielectric::new(1.5));
     // world
     let mut world = HittableList::new();
-    world.add(Rc::new(Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0, Rc::clone(&matte_green))));
-    world.add(Rc::new(Sphere::new(Point::new(0.0, 0.0, -1.0), 0.5, Rc::clone(&matte_red))));
-    world.add(Rc::new(Sphere::new(Point::new(-1.0, 0.0, -1.0), 0.5, Rc::clone(&metal_grey))));
-    world.add(Rc::new(Sphere::new(Point::new(1.0, 0.0, -1.0), 0.5, Rc::clone(&metal_blue))));
+    world.add(Rc::new(Sphere::new(
+        Point::new(0.0, -100.5, -1.0),
+        100.0,
+        Rc::clone(&matte_green),
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point::new(0.0, 0.0, -1.0),
+        0.5,
+        Rc::clone(&matte_purple),
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point::new(-1.0, 0.0, -1.0),
+        0.5,
+        Rc::clone(&metal_grey),
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point::new(1.0, 0.0, -1.0),
+        0.5,
+        Rc::clone(&glass),
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point::new(1.0, 1.0, -3.0),
+        1.0,
+        Rc::clone(&metal_blue),
+    )));
+
 
     let cam = Camera::new();
 
@@ -74,7 +104,7 @@ fn main() {
                 let u = (i as f64 + random_double(0.0, 1.0)) / ((image_width - 1) as f64);
                 let v = (j as f64 + random_double(0.0, 1.0)) / ((image_height - 1) as f64);
                 let ray = cam.get_ray(u, v);
-                pixel_color += ray_color(&ray,&world, max_depth);
+                pixel_color += ray_color(&ray, &world, max_depth);
             }
             write_color(pixel_color, samples_per_pixel);
         }
