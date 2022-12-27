39c5350773816f1a3d481c6efc9a9a5b0cde7b7b
Tony Zhao <tawnyzhao@gmail.com>
2022-12-26 18:06:10 -0800

    Defocus blur

--- src/camera.rs
+++ src/camera.rs
@@ -1,7 +1,7 @@
 use crate::{
     ray::Ray,
     utils::degrees_to_radians,
-    vec3::{Point, Vec3, unit_vector, cross},
+    vec3::{Point, Vec3, unit_vector, cross, random_in_unit_disk},
 };
 
 pub struct Camera {
@@ -9,6 +9,12 @@ pub struct Camera {
     lower_left_corner: Point,
     horizontal: Vec3,
     vertical: Vec3,
+
+    w: Vec3,
+    u: Vec3,
+    v: Vec3,
+
+    lens_radius: f64,
 }
 
 impl Camera {
@@ -18,36 +24,43 @@ impl Camera {
         v_up: Vec3,
         vertical_fov: f64,
         aspect_ratio: f64,
+        aperture: f64,
+        focus_distance: f64
     ) -> Self {
         let theta = degrees_to_radians(vertical_fov);
         let h = f64::tan(theta / 2.0);
-
         let viewport_height = 2.0 * h;
         let viewport_width = aspect_ratio * viewport_height;
-        let focal_length = 1.0;
         
         let w = unit_vector(look_from - look_at);
         let u = unit_vector(cross(v_up, w));
         let v = cross(w, u);
 
         let origin = look_from;
-        let horizontal = viewport_width * u;
-        let vertical = viewport_height * v;
+        let horizontal = focus_distance * viewport_width * u;
+        let vertical = focus_distance * viewport_height * v;
         let lower_left_corner =
-            origin - horizontal / 2.0 - vertical / 2.0 - w;
-            
+            origin - horizontal / 2.0 - vertical / 2.0 - focus_distance * w;
+
         Camera {
             origin,
             lower_left_corner,
             horizontal,
             vertical,
+            w,
+            u,
+            v,
+            lens_radius: aperture / 2.0,
         }
     }
 
     pub fn get_ray(&self, s: f64, t: f64) -> Ray {
+        let rd = self.lens_radius * random_in_unit_disk();
+        let offset = self.u * rd.x() + self.v * rd.y();
+
         Ray::new(
-            self.origin,
-            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin,
+            self.origin + offset,
+            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin - offset,
         )
     }
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -95,13 +95,23 @@ pub fn random_in_hemisphere(normal: Vec3) -> Vec3 {
     }
 }
 
+pub fn random_in_unit_disk() -> Vec3 {
+    loop {
+        let p = Vec3::new(random_double(-1.0, 1.0), random_double(-1.0, 1.0), 0.0);
+
+        if p.length_squared() < 1.0 {
+            return p;
+        }
+    }
+}
+
 pub fn reflect(v: Vec3, n: Vec3) -> Vec3 {
-    v - 2.0*dot(v,n)*n
+    v - 2.0 * dot(v, n) * n
 }
 
 pub fn refract(uv: Vec3, n: Vec3, etai_over_etat: f64) -> Vec3 {
     let cos_theta = f64::min(dot(-uv, n), 1.0);
-    let r_out_perp = etai_over_etat * (uv + cos_theta*n);
+    let r_out_perp = etai_over_etat * (uv + cos_theta * n);
     let r_out_parallel = -f64::sqrt(f64::abs(1.0 - r_out_perp.length_squared())) * n;
     r_out_perp + r_out_parallel
 }
